18c56a22a6b4bf6f7298a8220113e67626aeb4c9
LightIn_ <breval2000@live.fr>
2021-10-05 11:42:20 +0200

    css for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import {LoginComponent} from './components/authentification/login/login.componen
 import {ReactiveFormsModule} from "@angular/forms";
 import { RegisterComponent } from './components/authentification/register/register.component';
 import { ProfileComponent } from './components/authentification/profile/profile.component';
+import {BaseUnderTitleComponent} from "./components/core/base/base-under-title/base-under-title.component";
+import { LandingComponent } from './components/landing/landing.component';
+import {CoursesCreatesComponent} from "./components/courses/courses-creates/courses-creates.component";
+import { CoursesSuggestsComponent } from './components/courses/courses-suggests/courses-suggests.component';
+import { ButtonRouteurComponent } from './components/core/button-routeur/button-routeur.component';
+import {CoursesRegistrationsComponent} from "./components/courses/courses-registrations/courses-registrations.component";
 
 @NgModule({
   declarations: [
@@ -17,6 +23,15 @@ import { ProfileComponent } from './components/authentification/profile/profile.
     LoginComponent,
     RegisterComponent,
     ProfileComponent,
+    BaseUnderTitleComponent,
+    BaseComponent,
+    LandingComponent,
+    CoursesCreatesComponent,
+    CoursesSuggestsComponent,
+    ButtonRouteurComponent,
+    CoursesRegistrationsComponent
+
+
   ],
   imports: [
     BrowserModule,
