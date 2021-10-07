60f6cf49b48e49b488e6123a6c48e02755c353bf
Cinquin Andy <cinquin.andy@gmail.com>
2021-10-07 14:37:04 +0200

    course create fix responsive - add elements "for"

--- src/app/components/courses/courses-creates/courses-creates-form/courses-creates-form.component.ts
+++ src/app/components/courses/courses-creates/courses-creates-form/courses-creates-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject} from "../../../../interfaces/subject";
+import {Classe} from "../../../../interfaces/classe";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-courses-creates-form',
@@ -6,10 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./courses-creates-form.component.scss']
 })
 export class CoursesCreatesFormComponent implements OnInit {
+  subjectslist ?: Array<Subject>;
+  classesList ?: Array<Classe>
 
-  constructor() { }
+  form: FormGroup;
+
+  submit() {
+    // todo submit
+    console.log(this.form.status==="VALID")
+    console.log(this.form.status==="INVALID")
+    console.log(this.form.value)
+    //  todo call api
+  }
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      // todo validator personalized
+      title: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+      subjects: ['', [Validators.required]],
+      classes: ['', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
+    this.subjectslist = [
+      {
+        id:1,
+        title:"premier"
+      },
+      {
+        id:2,
+        title:"deuxième"
+      },
+      {
+        id:3,
+        title:"troisième"
+      }
+    ]
+    this.classesList = [
+      {
+        id:1,
+        title:"premier"
+      },
+      {
+        id:2,
+        title:"deuxième"
+      },
+      {
+        id:3,
+        title:"troisième"
+      }
+    ]
   }
 
 }
