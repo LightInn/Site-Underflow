bd7c52ca2e60d0cc271ce00902aa17f124dd3ce5
Cinquin Andy <cinquin.andy@gmail.com>
2021-10-09 00:01:20 +0200

    fixs elements

--- src/app/components/courses/courses-registrations/courses-registrations-form/courses-registrations-form.component.ts
+++ src/app/components/courses/courses-registrations/courses-registrations-form/courses-registrations-form.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Courses} from "../../../../interfaces/course";
-import {CourseSubscription} from "../../../../interfaces/CourseSubscription";
+import {CourseSubscription} from "../../../../interfaces/courseSubscription";
+import {Classe} from "../../../../interfaces/classe";
+import {from} from "rxjs";
+import {filter} from "rxjs/operators";
+import {CardState} from "../../../../interfaces/cardState";
 
 @Component({
   selector: 'app-courses-registrations-form',
@@ -10,26 +14,40 @@ import {CourseSubscription} from "../../../../interfaces/CourseSubscription";
 export class CoursesRegistrationsFormComponent implements OnInit {
   coursesList?: Array<Courses>;
   courseInscription?: Array<CourseSubscription>;
+  classesList?: Array<Classe>;
+  displayable?:Array<CardState> = [];
 
   elemCheck(id?: number) {
-    // console.log("----------------------------")
-    // console.log("elem check" + id)
     // if id is defined we test it
     if (!!id) {
-      // console.log("true elem" + id)
-      // // if the id is present in courseInscription
-      // // @ts-ignore
-      // console.log(!!(this.courseInscription.find(({id_course}) => id_course === id)));
-      // // @ts-ignore
-      // console.log(this.courseInscription.find(({id_course}) => id_course === id));
       // @ts-ignore
       return !!(this.courseInscription.find(({id_course}) => id_course === id));
     }
-    // console.log("false elem" + id)
     return false
   }
 
-  constructor() {}
+  callFilter(){
+    console.log(this.classesList);
+    // @ts-ignore
+    const courses$ = from(this.classesList);
+    console.log(courses$)
+    const filteredClasses$ = courses$
+      .pipe(filter(classes => classes.title==="B3"));
+    const subscribe = filteredClasses$.subscribe(val =>console.log(val))
+
+    // console.log(subscribe)
+    // const source = from([
+    //   { name: 'Joe', age: 31 },
+    //   { name: 'Bob', age: 25 }
+    // ]);
+    // //filter out people with age under 30
+    // const example = source.pipe(filter(person => person.age >= 30));
+    // //output: "Over 30: Joe"
+    // const subscribe = example.subscribe(val => console.log(`Over 30: ${val.name}`));
+  }
+  constructor() {
+
+  }
 
   clickEvent(id?: number) {
     // @ts-ignore
@@ -44,6 +62,20 @@ export class CoursesRegistrationsFormComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.classesList = [
+      {
+        id: 1,
+        title: "B1"
+      },
+      {
+        id: 2,
+        title: "B2"
+      },
+      {
+        id: 3,
+        title: "B3"
+      }
+    ]
     this.courseInscription = [
       {
         "id_user": "981477da-31c3-4887-b98f-6f9cc0f44e40",
@@ -140,5 +172,19 @@ export class CoursesRegistrationsFormComponent implements OnInit {
         "title": "Cours JS"
       }
     ]
+    for (const course of this.coursesList) {
+      // if the course is already push with the state -> then just skip the process
+      console.log("init courses");
+      console.log(course.id);
+      // @ts-ignore
+      console.log(this.displayable.find(id_course=>id_course===course.id))
+      // // @ts-ignore
+      // if(!!(this.displayable.find(id_course=>id_course===course.id)))
+      // {
+      //   // @ts-ignore
+      //   this.displayable.push({id:course.id,state:true})
+      // }
+    }
+    this.callFilter();
   }
 }
---
+++ src/app/interfaces/cardState.ts
@@ -0,0 +1,4 @@
+export interface CardState {
+  id?: number
+  state?:boolean|null
+}
