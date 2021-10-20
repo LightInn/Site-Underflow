b5bd0fdfeeffba28bf6ac6188b246c78dc6aeca9
CinquinAndy <cinquin.andy@gmail.com>
2021-10-20 15:31:30 +0200

    validatators and others stuffs like that on suggestions

--- src/app/components/courses/courses-suggests/courses-suggests-form/courses-suggests-form.component.ts
+++ src/app/components/courses/courses-suggests/courses-suggests-form/courses-suggests-form.component.ts
@@ -5,6 +5,7 @@ import {Classe} from "../../../../interfaces/classe";
 import {AuthentificationService} from "../../../../services/authentification.service";
 import {ToastService} from "../../../../services/toast.service";
 import {Router} from "@angular/router";
+import {toFormDateLocaleString} from "../../../../functions/dateFormat";
 
 @Component({
   selector: 'app-courses-suggests-form',
@@ -12,21 +13,44 @@ import {Router} from "@angular/router";
   styleUrls: ['./courses-suggests-form.component.scss']
 })
 export class CoursesSuggestsFormComponent implements OnInit {
+  // *************** Declaration part ******************* //
   subjectslist ?: Array<Subject>;
   classesList ?: Array<Classe>
   form: FormGroup;
+  /**
+   * We limit here & in the constructor the field for date
+   * -> 120 day after for the max
+   * -> now for the min limit
+   */
+  date_min: string = toFormDateLocaleString(new Date(Date.now()));
+  date_max: string;
 
+  /**
+   * The constructor, take differents parameters, it's automatic in angular,
+   * we use differents values with it
+   * @param fb -> to create our form, and initialize Validators
+   * @param authService -> to use the auth service
+   * @param toastService -> to display messages with popup service
+   * @param router -> to swap on another page on submit
+   */
   constructor(private fb: FormBuilder,
               private authService: AuthentificationService,
               private toastService: ToastService,
               private router: Router) {
     this.form = this.fb.group({
-      // todo validator personalized
       title: ['', [Validators.required]],
       date_butoir: ['', [Validators.required]],
       subjects: ['', [Validators.required]],
       classes: ['', [Validators.required]],
     });
+    /**
+     * we set the limit here for the max value
+     * change de "120" value to an another value if you want more flexibility on date
+     * for the suggestions
+     */
+    var date = new Date(Date.now());
+    date.setDate(date.getDate() + 120);
+    this.date_max = toFormDateLocaleString(date);
   }
 
   ngOnInit(): void {
@@ -61,12 +85,16 @@ export class CoursesSuggestsFormComponent implements OnInit {
   }
 
   submit() {
-    // todo submit
-    console.log(this.form.status==="VALID")
-    console.log(this.form.status==="INVALID")
-    console.log(this.form.value)
 
-    this.toastService.newToast("Envoie de la suggestion...", true)
+    // todo submit
+    console.log(this.form.status === "VALID")
+    console.log(this.form.status === "INVALID")
+    console.log(this.form.value)
+    for (const control in this.form.controls) {
+      console.log(this.form.controls[control].errors)
+    }
+
+    this.toastService.newToast("Suggestion envoyée...", false);
     //  todo call api
   }
 }
