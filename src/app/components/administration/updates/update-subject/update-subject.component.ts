8bc146fe11b16b0345c1b51511c2d6c4b445d0f8
Cinquin Andy <andy.cinquin@kleegroup.com>
2021-10-20 13:01:12 +0200

    push updates on subjects

--- src/app/components/administration/updates/update-subject/update-subject.component.ts
+++ src/app/components/administration/updates/update-subject/update-subject.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthentificationService} from "../../../../services/authentification.service";
+import {ToastService} from "../../../../services/toast.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-update-subject',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update-subject.component.scss']
 })
 export class UpdateSubjectComponent implements OnInit {
+  form: FormGroup;
+  subjectId: string | null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              private authService: AuthentificationService,
+              private toastService: ToastService,
+              private router: Router,
+              private route : ActivatedRoute) {
+    this.form = this.fb.group({
+      // todo validator personalized
+      id: ['', [Validators.required]],
+      title: ['', [Validators.required]],
+    });
+    this.subjectId = this.route.snapshot.paramMap.get('id');
   }
 
+  ngOnInit(): void {
+
+  }
+
+  submit() {
+    // todo submit
+    console.log(this.form.status === "VALID");
+    console.log(this.form.status === "INVALID");
+    console.log(this.form.value);
+
+    console.log(this.subjectId);
+
+    this.toastService.newToast("Update subject...", true)
+    //  todo call api
+  }
 }
