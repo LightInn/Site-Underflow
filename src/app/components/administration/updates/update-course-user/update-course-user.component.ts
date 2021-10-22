61476c8960a6a41267e28050758a504038f0c1d2
CinquinAndy <cinquin.andy@gmail.com>
2021-10-22 20:50:33 +0200

    course update for the user who created

---
+++ src/app/components/administration/updates/update-course-user/update-course-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-course-user',
+  templateUrl: './update-course-user.component.html',
+  styleUrls: ['./update-course-user.component.scss']
+})
+export class UpdateCourseUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
