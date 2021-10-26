052adf8babdd84a449bece4fe808f3f7978cf2a5
Breval Le Floch <breval2000@live.fr>
2021-10-26 12:40:34 +0200

    Auto - Disconnecte

--- src/app/interceptors/authentification.interceptor.ts
+++ src/app/interceptors/authentification.interceptor.ts
@@ -1,13 +1,10 @@
 import {Injectable} from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, of, throwError} from 'rxjs';
 import {AuthentificationService} from "../services/authentification.service";
-import {switchMap} from "rxjs/operators";
+import {catchError, switchMap} from "rxjs/operators";
+import {Router} from "@angular/router";
+import {ToastService} from "../services/toast.service";
 
 
 @Injectable()
@@ -15,11 +12,30 @@ export class AuthentificationInterceptor implements HttpInterceptor {
   // *************** Declaration part ******************* //
   private static CSRFTokenRun: boolean;
 
-  constructor(private authService: AuthentificationService) {
+  constructor(private authService: AuthentificationService,
+              private router: Router,
+              private toastService: ToastService
+  ) {
     if (AuthentificationInterceptor.CSRFTokenRun == null) {
       AuthentificationInterceptor.CSRFTokenRun = false;
     }
   }
+
+
+  private handleAuthError(err: HttpErrorResponse): Observable<any> {
+    //handle your auth error or rethrow
+    if (err.status === 401 || err.status === 403) {
+      //navigate /delete cookies or whatever
+      this.authService.logout();
+      this.router.navigateByUrl(`/login`);
+      this.toastService.newToast("Vous avez été deconecter", true)
+
+      // if you've caught / handled the error, you don't want to rethrow it unless you also want downstream consumers to have to handle it as well.
+      return of(err.message); // or EMPTY may be appropriate here
+    }
+    return throwError(err);
+  }
+
 
   intercept(req: HttpRequest<any>,
             next: HttpHandler): Observable<HttpEvent<any>> {
@@ -43,7 +59,8 @@ export class AuthentificationInterceptor implements HttpInterceptor {
         clone = req.clone();
       }
       AuthentificationInterceptor.CSRFTokenRun = false;
-      return next.handle(clone);
+      return next.handle(clone).pipe(catchError(x => this.handleAuthError(x)));
+
     } else {
       AuthentificationInterceptor
         .CSRFTokenRun = true;
@@ -55,7 +72,7 @@ export class AuthentificationInterceptor implements HttpInterceptor {
               headers: req.headers.set("X-CSRFToken", AuthentificationService.csrfToken)
                 .set("Authorization", "Bearer " + (idToken === null ? "" : idToken))
             })
-          return next.handle(clone);
+          return next.handle(clone).pipe(catchError(x => this.handleAuthError(x)));
         }
       ));
     }
