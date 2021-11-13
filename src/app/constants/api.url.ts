9e1f192029374ae260f23e0f3bd47e7cbed48233
CinquinAndy <cinquin.andy@gmail.com>
2021-11-13 17:16:56 +0100

    change api url

--- src/app/constants/api.url.ts
+++ src/app/constants/api.url.ts
@@ -1,3 +1,3 @@
-// export const ApiUrl = "https://s2.scratchunderflow.fr:4242"
+export const ApiUrl = "https://api.scratchunderflow.fr/"
 // export const ApiUrl = "http://localhost:8009"
-export const ApiUrl = "http://127.0.0.1:5000"
+// export const ApiUrl = "http://127.0.0.1:5000"
