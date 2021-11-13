29f38ca01114928188c4cb871276a6b5f22f69cd
CinquinAndy <cinquin.andy@gmail.com>
2021-11-13 17:22:27 +0100

    re-oups

--- src/app/constants/api.url.ts
+++ src/app/constants/api.url.ts
@@ -1,3 +1,3 @@
-export const ApiUrl = "https://api.scratchunderflow.fr/"
+export const ApiUrl = "https://api.scratchunderflow.fr"
 // export const ApiUrl = "http://localhost:8009"
 // export const ApiUrl = "http://127.0.0.1:5000"
